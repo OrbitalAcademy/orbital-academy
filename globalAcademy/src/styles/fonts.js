7abd3f04b1566ee0241930bdfbaeeb77a738db52
mneugebauer-integration <mneugebauer@integrationconsulting.com.br>
2026-06-03 13:49:11 -0300

    add fonts

---
+++ globalAcademy/src/styles/fonts.js
@@ -0,0 +1,10 @@
+export const fonts = {
+  body: 'Nunito_400Regular',
+  bodySemiBold: 'Nunito_600SemiBold',
+  bodyBold: 'Nunito_700Bold',
+
+  title: 'Roboto_400Regular',
+  titleBold: 'Roboto_700Bold',
+  titleBlack: 'Roboto_900Black',
+};
+
